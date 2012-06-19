38b18fca9ee10af9e5e014b884725623c24473ae
Denis Marcaux <dmarcaux@rue89.com>
2012-06-19 11:12:28 +0200

    fixed haystack index bug

--- docviewer/search_indexes.py
+++ docviewer/search_indexes.py
@@ -1,13 +1,13 @@
 import datetime
-from haystack import indexes
+from haystack.indexes import *
 from docviewer.models import Page
 
 
-class PageIndex(indexes.RealTimeSearchIndex, indexes.Indexable):
+class PageIndex(RealTimeSearchIndex):
 
-    text = indexes.CharField(document=True)
-    document_id =  indexes.IntegerField(model_attr='document__id')
-    page = indexes.IntegerField(model_attr="page")
+    text = CharField(document=True)
+    document_id =  IntegerField(model_attr='document__id')
+    page = IntegerField(model_attr="page")
     
     def prepare_text(self, obj):
         return obj.text
